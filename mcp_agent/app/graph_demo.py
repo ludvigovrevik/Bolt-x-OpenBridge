323d5d040cba334647e87e5e1c136fc90c6249e7
TeodorRusKvi <teodor.ruskvi@gmail.com>
2025-04-17 09:28:41 +0200

    halla

--- mcp_agent/app/graph_demo.py
+++ mcp_agent/app/graph_demo.py
@@ -249,7 +249,7 @@ async def get_replan_step():
         
         replanner_message = replanner_prompt.format_messages(
             input=input_content,
-            design_template=state.design_template,
+            design_template=get_design_template(openbridge_example),
             original_plan=original_plan,
             past_steps=past_steps_formatted
         )
